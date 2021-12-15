0b10d63217ab33d298992116aaacecfd368c1134
yuka2875 <yuka986532@gmail.com>
2021-12-15 16:03:09 +0900

    モデルの単体テストを実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'000000'}
+    password_confirmation {password}
+    last_name             {'全角'}
+    last_name_kana        {'ゼンカク'}
+    first_name            {'全角'}
+    first_name_kana       {'ゼンカク'}
+    birth_date            {'1990-01-05'}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,130 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+
+    #上手くいく時と行かない時を使い分ける。同じ項目を同じ場合に書くとエラー文が重複して表示されない
+
+    context '新規登録が上手く行く時' do
+      it "nicknameとemail、passwordとpassword_confiramation,last_name,first_name,last_name_kana,first_name_kana,birth_dateが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "passwordが６文字以上の半角英数字混合であれば登録できる" do
+        @user.password = "123abc"
+        @user.password_confirmation = "123abc"
+        expect(@user).to be_valid
+      end
+      it "last_nameが全角であれば登録できる" do
+        @user.last_name = "全角"
+        expect(@user).to be_valid
+      end
+      it "first_nameが全角であれば登録できる" do
+        @user.first_name = "全角"
+        expect(@user).to be_valid
+      end
+      it "last_name_kanaが全角カナであれば登録できる" do
+        @user.last_name_kana = "ゼンカク"
+        expect(@user).to be_valid
+      end
+      it "first_name_kanaが全角カナであれば登録できる" do
+        @user.first_name_kana = "ゼンカク"
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録が上手くいかない時' do
+    
+    it "nicknameが空では登録できない" do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+
+    it "emailが空では登録できない" do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    
+    it 'emailアドレスが一意性でないと登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+
+    it "emailアドレスに＠を含まなければ登録できない" do
+    @user.email = 'fukumanaigmail.com'
+    @user.valid?
+    expect(@user.errors.full_messages).to include("Email is invalid")
+    end
+
+    it "passwordが空では登録できない" do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'passwordが5文字以下であれば登録できない' do
+      @user.password = '12345'
+      @user.password_confirmation = '12345'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
+
+  
+    
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "last_nameが空では登録できない" do
+      @user.last_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end  
+
+   
+    it "first_nameが空では登録できない" do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end  
+
+    
+    it "last_name_kanaが空では登録できない" do
+      @user.last_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+    end  
+
+    
+
+    it "first_name_kanaが空では登録できない" do
+      @user.first_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
+    end  
+
+    
+    it "birth_dateが空では登録できない" do
+      @user.birth_date = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth date can't be blank")
+    end
+  end
+end 
+end
+
+
+
+
+
+    
