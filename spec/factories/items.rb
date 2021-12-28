589f486d9dff1b8d830044d8d55d8b401ba77365
yuka2875 <yuka986532@gmail.com>
2021-12-28 19:19:33 +0900

    修正1

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,6 +8,8 @@ FactoryBot.define do
     prefecture_id          { 2 }
     shipping_day_id        { 2 }
     price                  { '1000' }
+    association :user 
+
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Item, type: :model do
 
   describe '商品出品機能' do
     context '商品出品が上手く行く時' do
-      it 'item_name,image,explanation,category,item_status,delivery_cost,shipping_day,priceが存在すれば登録できる' do
+      it 'item_name,image,explanation,category,item_status,delivery_cost,shipping_day,price,userが存在すれば登録できる' do
         expect(@item).to be_valid
       end
 
@@ -66,11 +66,60 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
-      it 'priceは¥300〜¥9,999,999の間でなければ登録できない' do
+      it '価格が300円未満では出品できない' do
         @item.price = '100'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
+
+      it '価格が9,999,999円を超えると出品できない' do
+        @item.price = '10,000,000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
+      it '価格が半角数字以外が含まれている場合は出品できない' do
+        @item.price = '４00'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.item_status_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item status can't be blank")
+      end
+
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.delivery_cost_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery cost can't be blank")
+      end
+
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.shipping_day_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+
+      it 'userが紐付いていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User can't be blank")
+      end
+
     end
   end
 end
