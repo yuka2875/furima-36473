333b8da8b377e694862dd34cc52b836eabc8482a
yuka2875 <yuka986532@gmail.com>
2022-01-21 01:09:58 +0900

    修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :purchase_record
-  belongs_to :prefecture
 end
--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -53,5 +53,6 @@ class Prefecture < ActiveHash::Base
   include ActiveHash::Associations
   has_many :items
   has_many :addresses
+  has_many :purchase_record
 
 end
